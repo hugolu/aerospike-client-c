e7a990410620545f0ba9a70b7acd685969f1ba84
root <root@vagrant-debian-wheezy.vagrantup.com>
2015-08-26 03:58:12 +0000

    indent

--- asc/as2fs.c
+++ asc/as2fs.c
@@ -12,60 +12,58 @@
 
 void chkdir(char *path);
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  aerospike as;
-  char *ns, *key, *dir, file[512];
-  void *mem;
-  int size;
-  int fd;
-  int rc;
+    aerospike as;
+    char *ns, *key, *dir, file[512];
+    void *mem;
+    int size;
+    int fd;
+    int rc;
 
-  if (argc < 3) {
-    printf("Usage: %s <ns>:<key> <dir>\n", basename(argv[0]));
-    exit(-1);
-  }
-  ns = strtok(argv[1], ":");
-  key = strtok(NULL, ":");
-  dir = argv[2];
-  sprintf(file, "%s/%s", dir, key);
-  chkdir(file);
+    if (argc < 3) {
+        printf("Usage: %s <ns>:<key> <dir>\n", basename(argv[0]));
+        exit(-1);
+    }
+    ns = strtok(argv[1], ":");
+    key = strtok(NULL, ":");
+    dir = argv[2];
+    sprintf(file, "%s/%s", dir, key);
+    chkdir(file);
 
-  // Perpare the key
-  as_key as_key;
-  as_key_init_str(&as_key, ns, SET, key);
+    // Perpare the key
+    as_key as_key;
+    as_key_init_str(&as_key, ns, SET, key);
 
-  // Prepare the record
-	as_record *p_rec = NULL;
+    // Prepare the record
+    as_record *p_rec = NULL;
 
-  // read from database
-  as_init(&as);
-  as_read(&as, &as_key, &p_rec);
-  as_exit(&as);
+    // read from database
+    as_init(&as);
+    as_read(&as, &as_key, &p_rec);
+    as_exit(&as);
 
-  size = p_rec->bins.entries[0].value.bytes.size;
-  mem = p_rec->bins.entries[0].value.bytes.value;
+    size = p_rec->bins.entries[0].value.bytes.size;
+    mem = p_rec->bins.entries[0].value.bytes.value;
 
-  // write file
-  fd = creat(file, 0644);
-  if (fd < 0) {
-    ERROR("cannot open file %s", file);
-    return -1;
-  }
-  rc = write(fd, mem, size);
-  if (rc < 0) {
-    ERROR("cannot write file %s", file);
-    return -1;
-  }
-  close(fd);
+    // write file
+    fd = creat(file, 0644);
+    if (fd < 0) {
+        ERROR("cannot open file %s", file);
+        return -1;
+    }
+    rc = write(fd, mem, size);
+    if (rc < 0) {
+        ERROR("cannot write file %s", file);
+        return -1;
+    }
+    close(fd);
 
-  as_record_destroy(p_rec);
-  return 0;
+    as_record_destroy(p_rec);
+    return 0;
 }
 
-void
-chkdir(char *path)
+void chkdir(char *path)
 {
     char buf[512], *dir;
 
--- asc/fs2as.c
+++ asc/fs2as.c
@@ -10,57 +10,70 @@
 
 #include "as_utils.h"
 
-int
-main(int argc, char *argv[])
+int fs2as(aerospike *as, char *file, char *ns, char *key);
+
+int main(int argc, char *argv[])
 {
-  aerospike as;
-  char *file, *ns, *key;
-  struct stat st;
-  int fd;
-  void *mem;
-  int rc;
+    aerospike as;
+    char *file, *ns, *key;
 
-  if (argc < 3) {
-    printf("Usage: %s <file> <ns>:<key>\n", basename(argv[0]));
-    exit(-1);
-  }
-  file = argv[1];
-  ns = strtok(argv[2], ":");
-  key = strtok(NULL, ":");
+    if (argc < 3) {
+        printf("Usage: %s <file> <ns>:<key>\n", basename(argv[0]));
+        exit(-1);
+    }
+    file = argv[1];
+    ns = strtok(argv[2], ":");
+    key = strtok(NULL, ":");
 
-  rc = stat(file, &st);
-  if (rc < 0) {
-    ERROR("cannot stat file %s", file);
-    return -1;
-  }
+    // write to database
+    as_init(&as);
+    fs2as(&as, file, ns, key);
+    as_exit(&as);
 
-  fd = open(file, O_RDONLY);
-  if (fd < 0) {
-    ERROR("cannot open file %s", file);
-    return -1;
-  }
+    return 0;
+}
 
-  mem = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
-  if (mem == MAP_FAILED) {
-    ERROR("cannot mmap file %s", file);
-    return -1;
-  }
+int
+fs2as(aerospike *as, char *file, char *ns, char *key)
+{
+    struct stat st;
+    int fd, size;
+    void *mem;
+    int rc;
 
-  // Perpare the key
-  as_key as_key;
-  as_key_init_str(&as_key, ns, SET, key);
+    rc = stat(file, &st);
+    if (rc < 0) {
+        ERROR("cannot stat file %s", file);
+        return -1;
+    }
+    size = st.st_size;
 
-  // Prepare the record
-	as_record as_rec;
-	as_record_inita(&as_rec, 1);
-	as_record_set_raw(&as_rec, BIN, mem, st.st_size);
+    fd = open(file, O_RDONLY);
+    if (fd < 0) {
+        ERROR("cannot open file %s", file);
+        return -1;
+    }
 
-  // write to database
-  as_init(&as);
-  as_write(&as, &as_key, &as_rec);
-  as_exit(&as);
+    mem = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+    if (mem == MAP_FAILED) {
+        ERROR("cannot mmap file %s", file);
+        return -1;
+    }
 
-  munmap(mem, st.st_size);
-  close(fd);
-  return 0;
+    // Perpare the key
+    as_key as_key;
+    as_key_init_str(&as_key, ns, SET, key);
+
+    // Prepare the record
+    as_record as_rec;
+    as_record_inita(&as_rec, 1);
+    as_record_set_raw(&as_rec, BIN, mem, size);
+
+    // Write to DB
+    as_write(as, &as_key, &as_rec);
+
+    munmap(mem, size);
+    close(fd);
+
+    return 0;
 }
