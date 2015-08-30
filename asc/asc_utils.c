e6dbd3be2c6364d9ed4cc8514a2b1c2e89e64aae
root <root@vagrant-debian-wheezy.vagrantup.com>
2015-08-30 07:23:56 +0000

    asc support LDT

--- asc/asc_utils.c
+++ asc/asc_utils.c
@@ -37,6 +37,7 @@ bool asc_exit(aerospike *p_as)
     return (status == AEROSPIKE_OK);
 }
 
+
 bool asc_write(aerospike *p_as, char *ns, char *file, char *buf, uint32_t size)
 {
     as_key key;
@@ -51,7 +52,25 @@ bool asc_write(aerospike *p_as, char *ns, char *file, char *buf, uint32_t size)
     // Prepare the record
     as_record_inita(&rec, 2);
     as_record_set_int64(&rec, "size", size);
+
+#ifdef LDT_ENABLE
+    as_ldt lstack;
+    as_bytes bval;
+    as_ldt_init(&lstack, "data", AS_LDT_LSTACK, NULL);
+
+    uint32_t offset, length;
+    for (offset = 0; offset < size; offset += length) {
+        length = MIN(CHUNK_SIZE, size - offset);
+        as_bytes_init_wrap(&bval, (uint8_t *)buf + offset, length, false);
+        status = aerospike_lstack_push(p_as, &err, NULL, &key, &lstack, (const as_val*)&bval);
+        if (status != AEROSPIKE_OK) {
+            ERROR("aerospike_lstack_push() returned %d - %s", err.code, err.message);
+            break;
+        }
+    }
+#else
     as_record_set_raw(&rec, "data", (uint8_t *)buf, size);
+#endif
 
 RETRY:
     // Write the record to the database.
@@ -63,6 +82,63 @@ RETRY:
             goto RETRY;
         }
     }
+
+    return (status == AEROSPIKE_OK);
+}
+
+bool asc_read(aerospike *p_as, char *ns, char *file, char *buf, uint32_t size)
+{
+    as_key key;
+    as_record *rec = NULL;
+    as_status status;
+    as_error err;
+
+    // Prepare the key
+    as_key_init_str(&key, ns, SET, file);
+
+#ifdef LDT_ENABLE
+    as_ldt lstack;
+    as_ldt_init(&lstack, "data", AS_LDT_LSTACK, NULL);
+
+    uint32_t cnt;
+    status = aerospike_lstack_size(p_as, &err, NULL, &key, &lstack, &cnt);
+    if (status != AEROSPIKE_OK) {
+        ERROR("aerospike_lstack_size() returned %d - %s", err.code, err.message);
+        return false;
+    }
+
+    // Peek all the values back again.
+    as_list *p_list = NULL;
+    status = aerospike_lstack_peek(p_as, &err, NULL, &key, &lstack, cnt, &p_list);
+    if (status != AEROSPIKE_OK) {
+        ERROR("aerospike_lstack_peek() returned %d - %s", err.code, err.message);
+        return false;
+    }
+
+    // read lstack
+    const as_arraylist *p_array = (const as_arraylist *)p_list;
+    uint32_t i;
+    for (i = 0; i < cnt; i++) {
+        const as_val* p_val = p_array->elements[i];
+        const as_bytes* p_bytes = (const as_bytes*)p_val;
+        printf("size:%d\n", p_bytes->size);
+        memcpy(buf + (cnt-1-i)*CHUNK_SIZE, p_bytes->value, p_bytes->size);
+        printf("size:%d\n", p_bytes->size);
+    }
+    as_list_destroy(p_list);
+    p_list = NULL;
+#else
+    // Read the (whole) test record from the database.
+    status = aerospike_key_get(p_as, &err, NULL, &key, &rec);
+    if (status != AEROSPIKE_OK) {
+        ERROR("aerospike_key_get() returned %d - %s", err.code, err.message);
+    } else {
+        // Read the record
+        as_bytes *bytes = as_record_get_bytes(rec, "data");
+        memcpy(buf, bytes->value, MIN(bytes->size, size));
+    }
+    as_record_destroy(rec);
+#endif
 
     return (status == AEROSPIKE_OK);
 }
@@ -91,26 +167,3 @@ uint32_t asc_size(aerospike *p_as, char *ns, char *file)
     return size;
 }
 
-bool asc_read(aerospike *p_as, char *ns, char *file, char *buf, uint32_t size)
-{
-    as_key key;
-    as_record *rec = NULL;
-    as_status status;
-    as_error err;
-
-    // Prepare the key
-    as_key_init_str(&key, ns, SET, file);
-
-    // Read the (whole) test record from the database.
-    status = aerospike_key_get(p_as, &err, NULL, &key, &rec);
-    if (status != AEROSPIKE_OK) {
-        ERROR("aerospike_key_get() returned %d - %s", err.code, err.message);
-    } else {
-        // Read the record
-        as_bytes *bytes = as_record_get_bytes(rec, "data");
-        memcpy(buf, bytes->value, MIN(bytes->size, size));
-    }
-    as_record_destroy(rec);
-
-    return (status == AEROSPIKE_OK);
-}
--- asc/asc_utils.h
+++ asc/asc_utils.h
@@ -3,16 +3,25 @@
 
 #include <aerospike/aerospike.h>
 #include <aerospike/aerospike_key.h>
+#include <aerospike/aerospike_lstack.h>
+#include <aerospike/as_arraylist.h>
+#include <aerospike/as_arraylist_iterator.h>
 #include <aerospike/as_error.h>
+#include <aerospike/as_integer.h>
+#include <aerospike/as_ldt.h>
+#include <aerospike/as_list.h>
 #include <aerospike/as_record.h>
 #include <aerospike/as_status.h>
+#include <aerospike/as_val.h>
+
+#define LDT_ENABLE
+#define CHUNK_SIZE  (512*1024)
 
 #define HOST    "127.0.0.1"
 #define PORT    3000
 #define USER    ""
 #define PASS    ""
-#define SET     ""
-#define BIN     ""
+#define SET     "file"
 
 #define ERROR(FORMAT, ...) printf("[ERROR] " FORMAT "\n", __VA_ARGS__)
 #define WARN(FORMAT, ...) printf("[WARN] " FORMAT "\n", __VA_ARGS__)
